9d36a27a96b53c5739e81a4f188b51a72e42c376
ps1437 <praveen369soni@gmail.com>
2020-03-29 22:37:36 +0530

    first

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -15,7 +15,9 @@ const Navbar = (props) => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon">
+            
+          </span>
         </button>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
           <a className="navbar-brand" href="#">
