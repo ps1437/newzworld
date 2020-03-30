a79fe3d218ce4a8171308e7516e388d151b296ce
ps1437 <praveen369soni@gmail.com>
2020-03-30 11:20:23 +0530

    bug fixed

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -6,7 +6,11 @@ axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded'
   
 const http =  axios.create({
       
-    baseURL: "http://newsapi.org/v2/"
+    baseURL: "https://newsapi.org/v2/",
+    headers: {
+        'accept-language': 'en_US',
+        'content-type': 'application/x-www-form-urlencoded'
+    }
    
 });
 
